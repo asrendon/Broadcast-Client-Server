f99e0719cae0db53f37a8d802846a9d79c887418
asrendon <asrendon94@gmail.com>
2021-11-06 22:41:18 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,25 +1,15 @@
 import time
 import socket
-import ipaddress
-import netifaces as ni
 
-def getBroadcastIP():
-    ips=[]
-    ifaces = ni.interfaces()
-    for iface in ifaces:
-        try:
-            ips.append(ni.ifaddresses(iface)[ni.AF_INET][0]['broadcast'])
-        except:
-            pass
-    return ips
+
 
 def broadcastRelay(port):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.bind((getBroadcastIP()[0],port))
+    s.bind(('0.0.0.0',port))
     print("Listening for broadcasts on port: "+str(port))
     while True:
         addr,port=s.recvfrom(4096)[1]
         print("broadcast from "+str(addr)+" "+str(port))
         s.sendto(str.encode('this is testing'),(str(addr),port))
 
-broadcastRelay(12348)
+broadcastRelay(12349)
