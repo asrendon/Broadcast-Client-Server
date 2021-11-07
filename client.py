b8bff35b528665fc367842b172f52bf0ba90c70a
asrendon <asrendon94@gmail.com>
2021-11-06 22:41:36 -0400

    Update client.py

--- client.py
+++ client.py
@@ -1,15 +1,35 @@
-import time
 import socket
+import netifaces as ni
 
+def getBroadcastIP():
+    ips=[]
+    #ip  = socket.gethostbyname_ex(socket.gethostname())[2][0]  # Obtain your IP address
+    #net = ipaddress.IPv4Network(ip + '/' + netmask, False)
+    #broadcast = str(net.broadcast_address)
+    ifaces = ni.interfaces()
+    for iface in ifaces:
+        try:
+            print(ni.ifaddresses(iface)[ni.AF_INET])
+            ips.append(ni.ifaddresses(iface)[ni.AF_INET][0]["broadcast"])
+        except:
+            pass
+    return ips
 
-
-def broadcastRelay(port):
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.bind(('0.0.0.0',port))
-    print("Listening for broadcasts on port: "+str(port))
-    while True:
-        addr,port=s.recvfrom(4096)[1]
-        print("broadcast from "+str(addr)+" "+str(port))
-        s.sendto(str.encode('this is testing'),(str(addr),port))
-
-broadcastRelay(12348)
+def broadcast(port):
+    timeout=3
+    retry=21
+    server=""
+    s=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    s.bind(('0.0.0.0', 0))
+    s.settimeout(timeout)
+    for x in range(retry):
+        try:
+            for broad in getBroadcastIP():
+                s.sendto(str.encode('test'),(broad,port))
+            data,address=s.recvfrom(4096)
+            return address[0]
+        except socket.timeout:
+            print("timeout")
+    
+print("server is at "+str(broadcast(12349)))
